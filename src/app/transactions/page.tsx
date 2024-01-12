d62ddd94ae5701473574f7ccf90a7f4bb8414401
Mateus Queirós <mateuscqueiros1@gmail.com>
2024-01-12 19:26:55 -0300

    transactions

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,3 +1,17 @@
 export default function Transactions() {
-  return <main className=""></main>;
+  const totalExpenses = 0;
+
+  return (
+    <div className="flex flex-col p-6 pt-10">
+      <div className="flex flex-row justify-between">
+        <h1 className="font-bold text-3xl mb-5">Transações</h1>
+        {/* Botão adicionar transação */}
+      </div>
+      <div className="flex flex-row flex-wrap gap-x-[300px] gap-y-10 justify-center w-full p-12 bg-contrast rounded-xl mb-10">
+        <div>
+          <span className="font-medium">Total de gastos:</span> R${totalExpenses}
+        </div>
+      </div>
+    </div>
+  );
 }
